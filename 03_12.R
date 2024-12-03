a4d63a72df7ce9818bc94a3f1b033fa8bc87f3a1
Nico-Casarin <nico.casarin@gmail.com>
2024-12-03 22:41:01 +0100

    second start

--- 03_12.R
+++ 03_12.R
@@ -10,7 +10,7 @@ df <- readLines('files/03.txt')
 
 sum <- 0
 
-df <- str_extract_all(df, "mul\\(\\d+(,\\d+)*\\)")
+df_clean <- str_extract_all(df, "mul\\(\\d+(,\\d+)*\\)")
 
 for (i in seq_along(df_clean)) {
   for (e in seq_along(df_clean)) {
@@ -24,3 +24,37 @@ for (i in seq_along(df_clean)) {
 }
 print(sum)
 
+df_clean <- str_extract_all(df, "mul\\(\\d+(,\\d+)*\\)|do\\(\\)|don't\\(\\)")
+
+sum <- 0
+
+do <- T
+
+for (i in seq_along(df_clean)) {
+  data <- df_clean[[i]]
+  print(data)
+  for (e in seq_along(data)) {
+    if (data[e] == "don't()") {
+      data[e] <- "0,0"
+      do <- F
+    } else if (data[e] == "do()"){
+      data[e] <- "0,0"
+      do <- T
+    }
+    if (do == T) {
+      data[e] <- gsub("mul\\(", "", data[e])
+      data[e] <- gsub("\\)", "", data[e])
+    } else if (do == F) {
+      data[e] <- "0,0"
+    }
+  }
+  df_clean[[i]] <- data
+}
+
+for (i in seq_along(df_clean)) {
+  mult_vector <- sapply(df_clean[[i]], function(x) {
+    multiply_elements(x)
+  })
+  sum <- sum(mult_vector)+sum
+}
+sum
